5c5af861f161ee661c8905acfad7a26b634371df
Matan Maron <matanmaron@gmail.com>
2021-04-03 14:27:40 +0300

    add animations. ground. jungle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,12 +10,24 @@ namespace DonkeyDrums.Core
         [SerializeField] float stopSpeed = 0.01f;
         [SerializeField] float maxSpeed = 2;
         [SerializeField] float speedBumps = 0.1f;
+        [SerializeField] float JumpForce = 2f;
+
+        private Animator animator = null;
+        private Rigidbody2D rigidbody2D = null;
         private float speed = 0f;
         private bool keyAlternate;
+        private bool isWalking = false;
+        private bool isGrounded = false;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow) && keyAlternate == false)
+            if (Input.GetKeyDown(KeyCode.RightArrow) && keyAlternate == false)
             {
                 speed += speedBumps;
                 keyAlternate = true;
@@ -41,6 +53,34 @@ namespace DonkeyDrums.Core
             {
                 transform.position += Vector3.right * speed * Time.deltaTime;
                 Debug.Log($"speed - {speed}");
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                isGrounded = false;
+                rigidbody2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            }
+            SetAnim();
+        }
+
+        private void SetAnim()
+        {
+            if (speed != 0 && !isWalking)
+            {
+                isWalking = true;
+                animator.SetBool("Walking", true);
+            }
+            else if (speed == 0 && isWalking)
+            {
+                isWalking = false;
+                animator.SetBool("Walking", false);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                isGrounded = true;
             }
         }
     }
